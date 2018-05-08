b44416bbb217f3f0ef6a36f860a2992961d20f5f
Fulvio Corno <fulvio.corno@polito.it>
2018-05-08 23:36:30 +0200

    Refactoring dei metodi di creazione degli archi in funzioni separate

--- src/it/polito/tdp/artsmia/model/Model.java
+++ src/it/polito/tdp/artsmia/model/Model.java
@@ -24,7 +24,6 @@ public class Model {
 		this.artObjects = dao.listObjects();
 		System.out.format("Oggetti caricati: %d oggetti\n", this.artObjects.size());
 
-
 		// Crea il grafo
 		this.graph = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
 
@@ -36,42 +35,18 @@ public class Model {
 		Graphs.addAllVertices(this.graph, this.artObjects);
 
 		// Aggiungi gli archi (con il loro peso)
-
-		/*
-		 * VERSIONE 3 - la più efficiente di tutte ** esegue una query unica (complessa,
-		 * ma una sola) con la quale ottiene in un sol colpo tutti gli archi del grafo
-		 */
-		// TODO: basata sulla query:
-//		SELECT eo1.object_id, count(eo2.exhibition_id), eo2.object_id
-//		FROM exhibition_objects eo1, exhibition_objects eo2
-//		WHERE eo1.exhibition_id=eo2.exhibition_id
-//		AND eo2.object_id>eo1.object_id
-//		GROUP BY eo1.object_id, eo2.object_id
-		
-		/*
-		 * VERSIONE 2 - più efficiente ** Utilizza il metodo listArtObjectAndCount per
-		 * fare una sola query per vertice, ottenendo in un solo colpo tutti gli archi
-		 * adiacenti a tale vertice (ed il relativo peso)
-		 */
-		for (ArtObject ao : this.artObjects) {
-			List<ArtObjectAndCount> connessi = dao.listArtObjectAndCount(ao);
-
-			for (ArtObjectAndCount c : connessi) {
-				ArtObject dest = new ArtObject(c.getArtObjectId(), null, null, null, 0, null, null, null, null, null, 0,
-						null, null, null, null, null); // l'oggetto "dest" è un ArtObject inizializzato in modo
-														// <i>lazy</i>, cioè solo con i campi utili per il calcolo di
-														// hashCode/equals. In questo modo può "impersonare" un vertice
-														// del grafo.
-				// System.out.format("(%d, %d) peso %d\n", ao.getId(), dest.getId(),
-				// c.getCount()) ;
-				Graphs.addEdge(this.graph, ao, dest, c.getCount());
-			}
-		}
+		addEdgesV2();
 		System.out.format("Grafo creato: %d vertici, %d archi\n", graph.vertexSet().size(), graph.edgeSet().size());
-		/* FINE VERSIONE 2 */
 
-		/* VERSIONE 1 - per nulla efficiente ** esegue una query per ogni coppia di vertici */
-		/*
+	}
+
+	/**
+	 * Aggiungi gli archi al grafo
+	 * 
+	 * VERSIONE 1 - per nulla efficiente ** esegue una query per ogni coppia di
+	 * vertici
+	 */
+	private void addEdgesV1() {
 		for (ArtObject aop : this.artObjects) {
 			for (ArtObject aoa : this.artObjects) {
 				if (!aop.equals(aoa) && aop.getId() < aoa.getId()) { // escludo coppie (ao, ao) per escludere i loop
@@ -86,8 +61,50 @@ public class Model {
 					}
 				}
 			}
-		} */
-		/* FINE VERSIONE 1 */
+		}
+	}
+
+	/**
+	 * Aggiungi gli archi al grafo
+	 * 
+	 * VERSIONE 2 - più efficiente ** Utilizza il metodo listArtObjectAndCount per
+	 * fare una sola query per vertice, ottenendo in un solo colpo tutti gli archi
+	 * adiacenti a tale vertice (ed il relativo peso)
+	 */
+	private void addEdgesV2() {
+		ArtsmiaDAO dao = new ArtsmiaDAO();
+		for (ArtObject ao : this.artObjects) {
+			List<ArtObjectAndCount> connessi = dao.listArtObjectAndCount(ao);
+
+			for (ArtObjectAndCount c : connessi) {
+				ArtObject dest = new ArtObject(c.getArtObjectId(), null, null, null, 0, null, null, null, null, null, 0,
+						null, null, null, null, null); // l'oggetto "dest" è un ArtObject inizializzato in modo
+														// <i>lazy</i>, cioè solo con i campi utili per il calcolo di
+														// hashCode/equals. In questo modo può "impersonare" un vertice
+														// del grafo.
+				// System.out.format("(%d, %d) peso %d\n", ao.getId(), dest.getId(),
+				// c.getCount()) ;
+				Graphs.addEdge(this.graph, ao, dest, c.getCount());
+			}
+		}
+
+	}
+
+	/**
+	 * Aggiungi gli archi al grafo
+	 * 
+	 * VERSIONE 3 - la più efficiente di tutte ** esegue una query unica (complessa,
+	 * ma una sola) con la quale ottiene in un sol colpo tutti gli archi del grafo
+	 * 
+	 */
+	private void addEdgesV3() {
+		// TODO: basata sulla query:
+		// SELECT eo1.object_id, count(eo2.exhibition_id), eo2.object_id
+		// FROM exhibition_objects eo1, exhibition_objects eo2
+		// WHERE eo1.exhibition_id=eo2.exhibition_id
+		// AND eo2.object_id>eo1.object_id
+		// GROUP BY eo1.object_id, eo2.object_id
+
 	}
 
 	private int exhibitionComuni(ArtObject aop, ArtObject aoa) {
